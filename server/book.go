fc4fca210baf5417abb08eaf65c1552fe20089df
Kodie <kodiegoodwin@gmail.com>
2017-02-06 15:54:40 -0800

    server: add BooksHandler logic for GET and POST

--- server/book.go
+++ server/book.go
@@ -1,17 +1,61 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/boltdb/bolt"
+	"github.com/gorilla/mux"
 )
 
+// api/${user}/book
 func BooksHandler(db *bolt.DB) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 
+		// Gets the id from the url
+		vars := mux.Vars(r)
+		user := vars["user"]
+
+		switch r.Method {
+		case "GET":
+			books, err := GetBooks(db, user)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			// Sets header, code, and marshal response
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(books)
+
+		case "POST":
+			var b Book
+			decoder := json.NewDecoder(r.Body)
+			err := decoder.Decode(&b)
+			if err != nil {
+				http.Error(w, "Post body not formatted properly", http.StatusBadRequest)
+				return
+			}
+
+			err = PutBook(db, user, &b)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			// Sets header, code, and marshal response
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+
+		default:
+			http.Error(w, fmt.Sprintf("method %s not allowed", r.Method), http.StatusMethodNotAllowed)
+		}
 	}
 }
 
+// api/${user}/book/${title}
 func BookHandler(db *bolt.DB) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 
