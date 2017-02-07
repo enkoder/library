45008cc0830a2d252da5805be922389486c55822
Kodie <kodiegoodwin@gmail.com>
2017-02-06 22:18:36 -0800

    cli/show: add by argument to show

--- cli/show.go
+++ cli/show.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 
 	"github.com/spf13/cobra"
 )
@@ -14,31 +15,48 @@ var ShowCmd = &cobra.Command{
 	Short: "Displays books in the library",
 	Long:  `Displays books in the library. Args "all", "unread" and "read" are supported`,
 	PreRunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 1 {
-			return fmt.Errorf("Must add an argument of 'all', 'unread', or 'reag'")
+		if len(args) == 0 {
+			return fmt.Errorf("Args format: show [all|read|unread] [by AUTHOR]")
 		}
 
 		if args[0] != "all" &&
 			args[0] != "unread" &&
 			args[0] != "read" {
-			return fmt.Errorf("Must add an argument of 'all', 'unread', or 'reag'")
+			return fmt.Errorf("Args format: show [all|read|unread] [by AUTHOR]")
+		}
+
+		if len(args) > 1 {
+			if args[1] != "by" {
+				return fmt.Errorf("Args format: show [all|read|unread] [by AUTHOR]")
+			}
+			if len(args[2]) != 3 {
+				return fmt.Errorf("Args format: show [all|read|unread] [by AUTHOR]")
+			}
 		}
 
 		return nil
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
-
-		var endpoint string
-		if args[0] == "all" {
-			endpoint = fmt.Sprintf("%s/%s/book", Url, User)
-		} else if args[0] == "read" {
-			endpoint = fmt.Sprintf("%s/%s/book?read=true", Url, User)
-		} else if args[0] == "unread" {
-			endpoint = fmt.Sprintf("%s/%s/book?read=false", Url, User)
+		// Use url package to make it easier to build the url
+		url, err := url.Parse(fmt.Sprintf("%s/%s/book", Url, User))
+		if err != nil {
+			return err
 		}
 
+		q := url.Query()
+		if args[0] == "read" {
+			q.Set("read", "true")
+		} else if args[0] == "unread" {
+			q.Set("read", "false")
+		}
+
+		if len(args) > 1 {
+			q.Set("by", args[2])
+		}
+		url.RawQuery = q.Encode()
+
 		// send the get request
-		resp, err := http.Get(endpoint)
+		resp, err := http.Get(url.String())
 		if err != nil {
 			return err
 		}
